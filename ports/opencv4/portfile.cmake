2c4d4e71cf4065e9b65697161cae8bb618ad374b
Varphone Wong <varphone@qq.com>
2023-06-14 11:57:07 +0800

    [opencv4] fix GetTempPath build error
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- ports/opencv4/portfile.cmake
+++ ports/opencv4/portfile.cmake
@@ -23,6 +23,7 @@ vcpkg_from_github(
       0011-remove-python2.patch
       0012-fix-zlib.patch
       0015-fix-freetype.patch
+      0019-fix-get-temp-path.patch
 )
 # Disallow accidental build of vendored copies
 file(REMOVE_RECURSE "${SOURCE_PATH}/3rdparty/openexr")
